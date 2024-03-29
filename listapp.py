9beff5bccd845b1bf9d3604a46a2e293bb1bff8e
altradCS <145883008+altradCS@users.noreply.github.com>
2024-03-29 09:26:48 +0700

    Update listapp.py

--- listapp.py
+++ listapp.py
@@ -14,12 +14,3 @@ def remove_duplicates(lst):
 
     return unique_lst
 
-
-#test Sample
-original_list = [1, 2, 3, 4, 5, 5, 6, 7]
-shuffled_list = custom_shuffle(original_list)
-print("Shuffled list:", shuffled_list)
-
-unique_list = remove_duplicates(shuffled_list)
-print("List with duplicates removed:", unique_list)
-
